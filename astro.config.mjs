88bdbc60f00734c79c74beb4196fac817e63dcb8
Ali Siddique <alisiddique10@hotmail.com>
2026-03-07 00:56:22 +0000

    A

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salinash.studio',
+
   vite: {
     plugins: [tailwindcss()],
     server: {
       allowedHosts: true
     }
   },
+
   server: {
     allowedHosts: true
-  }
+  },
+
+  integrations: [sitemap()]
 });
